773e60e42468e823c8e1a9ddd6f88c7c7f1446ce
TheStrider007 <lhssan.ahrouan17@gmail.com>
2021-06-15 17:30:51 +0100

    if statement

--- Main.c
+++ Main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
-   char userName[50];
+   char userInp[50];
+   char userName[] = "lhssan";
    int tries = 0;
 
    while (tries < 3)
    {
-      printf("Enter a string : ");
-      fgets(userName, sizeof userName, stdin);
-      printf("You entered: %s \n", userName);
-      tries++;
-      printf("%d \n",tries);
+      printf("Enter a username : ");
+      fgets(userInp, sizeof userInp, stdin);
+
+      if (userInp == userName )
+      {
+        
+         printf("Hello %s You're number is: %d \n",userName , 16);
+         exit(1);
+         
+      }
+      else if(userInp!=userName){ tries++; printf("%d \n",tries);};
+      
+      
    }
+   
    
   
    return(0);
