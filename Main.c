7b21ec232a0056b574658fff0fe131d08717c3db
TheStrider007 <lhssan.ahrouan17@gmail.com>
2021-06-15 14:37:49 +0100

    First Commit

---
+++ Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   
+   char userName[] = "lhssan";
+   int userNamber = 15;
+   
+   printf("Hello %s \n", userName);
+   printf("Your number is %d \n", userNamber);
+
+
+   return 0;
+}
